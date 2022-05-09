5976952bb30485ad2f3a400e24982f9f9c95267e
Chao Zhou <Czhou0k@gmail.com>
2022-05-08 22:11:09 -0400

    added route for contact page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import styles from './App.module.css';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
 import Project from './components/Project';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/project' element={<Project />} />
+        <Route path='/contact' element={<Contact />} />
       </Routes>
     </Router>
   );
