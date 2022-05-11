1108bbb218649e3da4e594ad072d77b22b7045b0
Chao Zhou <Czhou0k@gmail.com>
2022-05-11 14:49:11 -0400

    switched up layout a bit with text and image

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,13 +4,13 @@ import style from './Home.module.css';
 const Home = () => {
   return (
     <div className={style.background}>
+      <div className={style.shine}>Welcome To My Site</div>
       <div>
         <img
           className={style.image2}
           src='https://cdn.myanimelist.net/images/characters/10/322962.jpg'
           alt='Goblin Slayer'
         />
-        <div>Hello</div>
       </div>
     </div>
   );
