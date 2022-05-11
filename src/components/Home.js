f768a9faeebe5f7e22c64e0998a0e12c3b195139
Chao Zhou <Czhou0k@gmail.com>
2022-05-10 22:25:09 -0400

    got opacity working for background image

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,13 @@ import React from 'react';
 import style from './Home.module.css';
 
 const Home = () => {
-  return <div className={style.image}></div>;
+  return (
+    <div className={style.image}>
+      <div>
+        <h1>Hello</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
