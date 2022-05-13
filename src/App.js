5bb857dc65043b8e64d3e6339d646b72ce39a79c
Chao Zhou <Czhou0k@gmail.com>
2022-05-12 23:01:52 -0400

    added 2nd project image and link with figcaption

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import styles from './App.module.css';
+
 import NavBar from './components/NavBar';
 import Home from './components/Home';
 import Project from './components/Project';
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import style from './Project.module.css';
 import P1 from '../assets/images/P1.png';
+import P2 from '../assets/images/P2.png';
 
 const Project = () => {
   return (
@@ -23,11 +24,28 @@ const Project = () => {
           />
 
           <figcaption className={style.figcaption}>
-            An App That Help People Be More Money Conscious
+            An App That Help Users Be More Money Conscious
           </figcaption>
         </figure>
       </div>
-      <div className={style.text}>AnimeGen</div>
+      <div className={style.text}>
+        <a
+          href='https://animegenx.herokuapp.com/'
+          target='_blank'
+          rel='noreferrer'
+        >
+          AnimeGen
+        </a>
+      </div>
+      <div>
+        <figure className={style.figure}>
+          <img className={style.project_img} src={P2} alt='AnimeGen Project' />
+
+          <figcaption className={style.figcaption}>
+            An App That Can Help Users Decide What Anime To Watch
+          </figcaption>
+        </figure>
+      </div>
     </div>
   );
 };
