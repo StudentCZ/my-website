cb0b4bb1da864a85945a87f4d8818b7cec710d1d
Chao Zhou <Czhou0k@gmail.com>
2022-05-12 21:12:25 -0400

    added image folder and an image to project div

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import style from './Project.module.css';
+import P1 from '../assets/images/P1.png';
 
 const Project = () => {
-  return <div className={style.text}>My Projects</div>;
+  return (
+    <div>
+      <div className={style.text}>My Projects</div>
+      <div>
+        <img
+          className={style.project_img}
+          src={P1}
+          alt='Shoulda Coulda Project'
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Project;
