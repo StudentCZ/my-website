e56be601d4bbb141bda5dd5a64e481fcec1991f7
Chao Zhou <Czhou0k@gmail.com>
2022-05-12 21:39:54 -0400

    fixed css for figcaption and figure

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,17 +4,19 @@ import P1 from '../assets/images/P1.png';
 
 const Project = () => {
   return (
-    <div>
+    <div className={style.project_div}>
       <div className={style.text}>My Projects</div>
       <div>
-        <figure>
+        <figure className={style.figure}>
           <img
             className={style.project_img}
             src={P1}
             alt='Shoulda Coulda Project'
           />
 
-          <figcaption className={style.figcaption}>Hello</figcaption>
+          <figcaption className={style.figcaption}>
+            An App That Help People Be More money Conscious
+          </figcaption>
         </figure>
       </div>
     </div>
