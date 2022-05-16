2b0d3e21f202b7cbb53f90f83ff912e0967af265
Chao Zhou <Czhou0k@gmail.com>
2022-05-16 09:41:02 -0400

    added image of myself to images folder and also about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import style from './About.module.css';
+import WorkPic from '../assets/images/Workpic1.jpg';
 
 const About = () => {
-  return <div className={style.background}>About Me</div>;
+  return (
+    <div className={style.background}>
+      <div>
+        <img className={style.pic} src={WorkPic} alt='myself' />
+      </div>
+    </div>
+  );
 };
 
 export default About;
