fc8859eef2a24ce3112d176cbc643eb47861db7a
Chao Zhou <Czhou0k@gmail.com>
2022-05-16 10:59:39 -0400

    added some more text about myself

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,12 @@ const About = () => {
     <div className={style.background}>
       <div className={style.pic_div}>
         <img className={style.pic} src={WorkPic} alt='myself' />
-        <p className={style.p}>Hello there, thanks for checking out my site.</p>
+        <p className={style.p}>
+          Hello there, thanks for checking out my site. My name is Chao Zhou and
+          I am Software Engineer/Developer. I graduated from Fullstack Academy
+          where i spent 4 months learning the PERN stack along with other
+          technologies.
+        </p>
       </div>
     </div>
   );
