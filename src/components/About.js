6c47b324ef497c789e2ebfc6629fdbe9947dcfff
Chao Zhou <Czhou0k@gmail.com>
2022-05-21 17:26:23 -0400

    redoing the project, clear the about component and css

--- src/components/About.js
+++ src/components/About.js
@@ -3,21 +3,7 @@ import style from './About.module.css';
 import WorkPic from '../assets/images/Workpic1.jpg';
 
 const About = () => {
-  return (
-    <div className={style.background}>
-      <div className={style.pic_div}>
-        <img className={style.pic} src={WorkPic} alt='myself' />
-        <p className={style.p}>
-          Hello there, thanks for checking out my site. My name is Chao Zhou and
-          I am Software Engineer/Developer. I graduated from Fullstack Academy
-          where i spent 4 months learning the PERN stack along with other
-          technologies.
-        </p>
-      </div>
-
-      <div className={style.test}>Hello</div>
-    </div>
-  );
+  return <div className={style.background}>About Me</div>;
 };
 
 export default About;
