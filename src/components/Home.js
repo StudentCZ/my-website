23dc98dec465ee6ed73dd409e5f4535190f8a988
Chao Zhou <Czhou0k@gmail.com>
2022-05-23 16:25:42 -0400

    added image on home page and style for css

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import WorkPic from '../assets/images/Workpic1.jpg';
+import style from './Home.module.css';
 
 const Home = () => {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <div>
+        <img className={style.image} src={WorkPic} alt='myself' />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
