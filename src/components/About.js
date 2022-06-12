3c0b42f2fc8d8362b9061e3e4eede83d2503b18d
Chao Zhou <Czhou0k@gmail.com>
2022-06-11 21:56:31 -0400

    added redux logo and image home page and image folder

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import style from './About.module.css';
 import logo from '../assets/images/React.jpg';
+import logo2 from '../assets/images/redux.svg';
 
 const About = () => {
   return (
@@ -8,6 +9,7 @@ const About = () => {
       <h1 className={style.heading}>My Skills</h1>
       <div>
         <img src={logo} width='100' alt='react logo' />
+        <img src={logo2} width='100' alt='redux logo' />
       </div>
     </div>
   );
