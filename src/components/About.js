648395eb43f5adbe1d721307072ca470f017594f
Chao Zhou <Czhou0k@gmail.com>
2022-06-11 22:08:59 -0400

    added javascript logo to asset folder and about page

--- src/components/About.js
+++ src/components/About.js
@@ -3,6 +3,7 @@ import style from './About.module.css';
 import logo from '../assets/images/React.jpg';
 import logo2 from '../assets/images/redux.svg';
 import logo3 from '../assets/images/CSS.jpg';
+import logo4 from '../assets/images/JS.jpg';
 
 const About = () => {
   return (
@@ -11,7 +12,8 @@ const About = () => {
       <div>
         <img src={logo} width='100' alt='react logo' />
         <img src={logo2} width='100' alt='redux logo' />
-        <img src={logo3} width='100' alt='redux logo' />
+        <img src={logo3} width='100' alt='CSS logo' />
+        <img src={logo4} width='100' alt='Javascript logo' />
       </div>
     </div>
   );
