197bd02a3856e05dce2212a46fffaf5b8c8f3b7a
Chao Zhou <Czhou0k@gmail.com>
2022-06-18 13:51:15 -0400

    made a function to make every progress bar responsive at the same time when clicking a button

--- src/components/About.js
+++ src/components/About.js
@@ -7,12 +7,20 @@ import logo4 from '../assets/images/JS.jpg';
 import logo5 from '../assets/images/Postgre.jpg';
 import logo6 from '../assets/images/Express.jpg';
 import logo7 from '../assets/images/HTML.jpg';
-import { move, move2, move3 } from './ProgressBar';
+import { move, move2, move3, move4 } from './ProgressBar';
+
+// function allBars() {
+//   move();
+//   move2();
+//   move3();
+//   move4();
+// }
 
 const About = () => {
   return (
     <div className={style.background}>
       <h1 className={style.heading}>My Skills</h1>
+      {/* <button onClick={() => allBars()}>All Progress</button> */}
       <div>
         <img src={logo} width='100' alt='react logo' />
         <img src={logo2} width='100' alt='redux logo' />
@@ -73,7 +81,7 @@ const About = () => {
         <h2 className={style.text}>CSS</h2>
         <div className='w3-light-grey' style={{ width: '50%' }}>
           <div
-            id='myBar2'
+            id='myBar4'
             className='w3-container w3-green w3-center'
             style={{ width: '0%' }}
           >
@@ -81,7 +89,7 @@ const About = () => {
           </div>
         </div>
         <br />
-        <button className='w3-button w3-green' onClick={() => move2()}>
+        <button className='w3-button w3-green' onClick={() => move4()}>
           Click Me
         </button>
       </div>
--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -33,6 +33,21 @@ export function move3() {
   var width = 0;
   var id = setInterval(frame, 50);
   function frame() {
+    if (width >= 80) {
+      clearInterval(id);
+    } else {
+      width++;
+      elem.style.width = width + '%';
+      elem.innerHTML = width * 1 + '%';
+    }
+  }
+}
+
+export function move4() {
+  var elem = document.getElementById('myBar4');
+  var width = 0;
+  var id = setInterval(frame, 50);
+  function frame() {
     if (width >= 70) {
       clearInterval(id);
     } else {
