e08d31c75abcebcb940385c2d0846aec3bd0b5eb
Chao Zhou <Czhou0k@gmail.com>
2022-06-18 20:23:24 -0400

    fixed heading in contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,11 @@ import React from 'react';
 import style from './Contact.module.css';
 
 const Contact = () => {
-  return <div>Contact Page</div>;
+  return (
+    <div className={style.contact}>
+      <h1>Contact</h1>
+    </div>
+  );
 };
 
 export default Contact;
