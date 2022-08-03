45117510c014c76f3458a44ce03f94b6f9fb3f69
Chao Zhou <Czhou0k@gmail.com>
2022-08-02 21:12:18 -0400

    split about page into 2 columns

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -7,7 +7,9 @@ const Photo = () => {
     <div className={style.background}>
       <div className={style.imageTextDiv}>
         <img className={style.image} src={WorkPic} alt='myself' />
-        <div className={style.imageText}>Hello</div>
+        <div>
+          <h1 className={style.text}>Hello</h1>
+        </div>
       </div>
       {/* <Project /> */}
     </div>
