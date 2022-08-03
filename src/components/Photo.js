a4c997be40973335355686f8191e10752fe7e2a9
Chao Zhou <Czhou0k@gmail.com>
2022-08-02 21:26:41 -0400

    added div to create the two col approach

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -5,13 +5,14 @@ import style from './Photo.module.css';
 const Photo = () => {
   return (
     <div className={style.background}>
-      <div className={style.imageTextDiv}>
-        <img className={style.image} src={WorkPic} alt='myself' />
-        <div>
+      <div className={style.twoCol}>
+        <div className={style.col1}>
+          <img className={style.image} src={WorkPic} alt='myself' />
+        </div>
+        <div className={style.col2}>
           <h1 className={style.text}>Hello</h1>
         </div>
       </div>
-      {/* <Project /> */}
     </div>
   );
 };
