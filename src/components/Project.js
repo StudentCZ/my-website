5868d3fa2a83c3cb70109c8939e648f024fe599d
Chao Zhou <Czhou0k@gmail.com>
2022-08-03 14:45:31 -0400

    moving skills icons to project component

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,12 +3,29 @@ import style from './Project.module.css';
 import P1 from '../assets/images/P1.png';
 import P2 from '../assets/images/P2.png';
 import Question from '../assets/images/Q.jpeg';
+import logo from '../assets/images/React.jpg';
+import logo2 from '../assets/images/redux.svg';
+import logo3 from '../assets/images/CSS.jpg';
+import logo4 from '../assets/images/JS.jpg';
+import logo5 from '../assets/images/Postgre.jpg';
+import logo6 from '../assets/images/Express.jpg';
+import logo7 from '../assets/images/HTML.jpg';
+import { move, move2, move3, move4, move5, move6, move7 } from './ProgressBar';
 
 const Project = () => {
   return (
     <div className={style.project_div}>
       <div>
         <h1>MY SKILLS AND PROJECT</h1>
+        <div>
+          <img src={logo} width='100' alt='react logo' />
+          <img src={logo2} width='100' alt='redux logo' />
+          <img src={logo3} width='100' alt='CSS logo' />
+          <img src={logo4} width='100' alt='Javascript logo' />
+          <img src={logo5} width='100' alt='PostgreSQL logo' />
+          <img src={logo7} width='100' alt='HTML logo' />
+          <img src={logo6} width='100' alt='Express logo' />
+        </div>
       </div>
       <div className={style.text}>
         <a
