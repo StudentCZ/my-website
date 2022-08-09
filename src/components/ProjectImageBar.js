4784855db9d9a7fd160022f380514537310e7b03
Chao Zhou <Czhou0k@gmail.com>
2022-08-09 11:22:44 -0400

    added a container for image bar component and the general setup

--- src/components/ProjectImageBar.js
+++ src/components/ProjectImageBar.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import style from './ProjectImageBar.module.css';
 
 const ProjectImageBar = () => {
-  return <div>Hello</div>;
+  return <div className={style.container}>T</div>;
 };
 
 export default ProjectImageBar;
