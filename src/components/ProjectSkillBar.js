a41bd7f7881868b3bdf8a57c1e3de454eb19145d
Chao Zhou <Czhou0k@gmail.com>
2022-08-09 10:44:28 -0400

    moved all the css for the skill bar to skill bar module

--- src/components/ProjectSkillBar.js
+++ src/components/ProjectSkillBar.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import style from './ProjectSkillBar.module.css';
 
 const ProjectSkillBar = () => {
-  return <div>Hello</div>;
+  return (
+    <div className={style.skillBar}>
+      <div className={style.skillContainer}>
+        <div className={style.skillCol1}>
+          <div>
+            <h2>Javascript</h2>
+            <div className='w3-light-grey w3-round'>
+              <div
+                className='w3-container w3-round w3-blue'
+                style={{ width: '25' }}
+              >
+                25%
+              </div>
+            </div>
+            <h2>HTML</h2>
+          </div>
+        </div>
+        <div className={style.skillCol2}>
+          <h2>React</h2>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProjectSkillBar;
