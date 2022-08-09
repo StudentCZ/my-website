98cb298a0007eff8dbe936ee548bff5db7aacefe
Chao Zhou <Czhou0k@gmail.com>
2022-08-09 10:41:05 -0400

    added project image and skill bar to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Photo from './Photo';
 import Project from './Project';
+import ProjectImageBar from './ProjectImageBar';
+import ProjectSkillBar from './ProjectSkillBar';
 
 const Home = () => {
   return (
     <div>
       <Photo />
       <Project />
+      <ProjectSkillBar />
+      <ProjectImageBar />
     </div>
   );
 };
---
+++ src/components/ProjectSkillBar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ProjectSkillBar = () => {
+  return <div>Hello</div>;
+};
+
+export default ProjectSkillBar;
