67f82cae0ce5a033e1624770131384308f0d8102
Chao Zhou <Czhou0k@gmail.com>
2022-08-09 19:57:35 -0400

    got text to be on top of the textbox

--- src/components/ProjectImageBar.js
+++ src/components/ProjectImageBar.js
@@ -2,7 +2,11 @@ import React from 'react';
 import style from './ProjectImageBar.module.css';
 
 const ProjectImageBar = () => {
-  return <div className={style.container}>T</div>;
+  return (
+    <div className={style.container}>
+      <div>Hello</div>
+    </div>
+  );
 };
 
 export default ProjectImageBar;
