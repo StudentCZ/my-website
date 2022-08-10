012ffc2e1470b5d92a59cd6c9fb77803611372a9
Chao Zhou <Czhou0k@gmail.com>
2022-08-09 21:25:44 -0400

    change display to flex on article page

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -4,7 +4,10 @@ import style from './Articles.module.css';
 const Articles = () => {
   return (
     <div className={style.background}>
-      <div className={style.title}>LATEST ARTICLES</div>
+      <div className={style.container}>
+        <div className={style.title}>LATEST ARTICLES</div>
+        <div className={style.text2}>All Articles</div>
+      </div>
     </div>
   );
 };
