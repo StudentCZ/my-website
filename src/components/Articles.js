dd57cd041cda546eb936efb1ee51280e89eb8c8d
Chao Zhou <Czhou0k@gmail.com>
2022-08-11 19:56:21 -0400

    added link to all article and removed line in hyperlink css

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -7,7 +7,15 @@ const Articles = () => {
       <div className={style.container}>
         <div className={style.title}>LATEST ARTICLES</div>
         <div className={style.textBox2}>
-          <h2 className={style.text2}>All Articles</h2>
+          <h2 className={style.text2}>
+            <a
+              href='https://medium.com/@Chao.Zhou'
+              rel='noreferrer'
+              target='_blank'
+            >
+              All Articles
+            </a>
+          </h2>
         </div>
       </div>
       <div>Hello</div>
