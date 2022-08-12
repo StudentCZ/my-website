b0593d521398c3a27c2680e9b92b01afefe3fd57
Chao Zhou <Czhou0k@gmail.com>
2022-08-11 20:55:16 -0400

    made a border box for contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ import style from './Contact.module.css';
 const Contact = () => {
   return (
     <div className={style.contact}>
-      <h1 className={style.heading}>Contacts</h1>
+      <div className={style.contactBox}></div>
     </div>
   );
 };
