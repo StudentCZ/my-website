2013777b0cbc64841ba9033b1d6bda0447ed50df
Chao Zhou <Czhou0k@gmail.com>
2022-08-12 19:19:53 -0400

    added another container for projectImageBar component

--- src/components/ProjectImageBar.js
+++ src/components/ProjectImageBar.js
@@ -3,10 +3,16 @@ import style from './ProjectImageBar.module.css';
 
 const ProjectImageBar = () => {
   return (
-    <div className={style.containerOne}>
-      <div>Hello</div>
-      <div>Hello</div>
-    </div>
+    <>
+      <div className={style.containerOne}>
+        <div>Hello</div>
+        <div>Hello</div>
+      </div>
+      <div className={style.containerTwo}>
+        <div>Hello</div>
+        <div>Hello</div>
+      </div>
+    </>
   );
 };
 
