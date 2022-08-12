5cd1584bfb26ba526460b02083994b33c27e488f
Chao Zhou <Czhou0k@gmail.com>
2022-08-11 20:37:01 -0400

    imported contact to home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ import Project from './Project';
 import ProjectImageBar from './ProjectImageBar';
 import ProjectSkillBar from './ProjectSkillBar';
 import Articles from './Articles';
+import Contact from './Contact';
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
       <ProjectSkillBar />
       <ProjectImageBar />
       <Articles />
+      <Contact />
     </div>
   );
 };
