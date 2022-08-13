c0c6fd1b09fdbedc16fdd8d67e3f27cbb3634ac7
Chao Zhou <Czhou0k@gmail.com>
2022-08-13 17:59:04 -0400

    changed display to flex in photo component and get rid of unused css class

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -5,10 +5,8 @@ import style from './Photo.module.css';
 const Photo = () => {
   return (
     <div className={style.background}>
-      <div className={style.twoCol}>
-        <div className={style.col1}>
-          <img className={style.image} src={WorkPic} alt='myself' />
-        </div>
+      <div className={style.container}>
+        <img className={style.image} src={WorkPic} alt='myself' />
         <div className={style.col2}>
           <div className={style.textBox}>
             <span className={style.text}>HI, I'M CHAO</span>
