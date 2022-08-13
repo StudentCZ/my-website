c2d00f2a7a6060cc76b3e7bf27b884f5a027b7a9
Chao Zhou <Czhou0k@gmail.com>
2022-08-12 21:05:51 -0400

    got rid of some css to mitagate repeating

--- src/components/ProjectImageBar.js
+++ src/components/ProjectImageBar.js
@@ -7,9 +7,9 @@ const ProjectImageBar = () => {
   return (
     <>
       <div className={style.containerOne}>
-        <img src={image1} alt='Project1' className={style.projectOneImage} />
+        <img src={image1} alt='Project1' className={style.projectImage} />
 
-        <div className={style.projectOneText}>
+        <div className={style.projectText}>
           <span className={style.header1}>SHOULDA COULDA</span>
           <p className={style.text1}>
             Shoulda Coulda is an app that helps users be more financial aware of
@@ -18,8 +18,8 @@ const ProjectImageBar = () => {
         </div>
       </div>
       <div className={style.containerTwo}>
-        <img src={image2} alt='Project2' className={style.projectTwoImage} />
-        <div>Hello</div>
+        <img src={image2} alt='Project2' className={style.projectImage} />
+        <div className={style.projectText}>Hello</div>
       </div>
     </>
   );
