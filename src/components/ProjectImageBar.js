53f7df09903aefd4c97ce16a43cae3fc37a769bb
Chao Zhou <Czhou0k@gmail.com>
2022-08-12 20:40:19 -0400

    added font family and change position of projectImage text

--- src/components/ProjectImageBar.js
+++ src/components/ProjectImageBar.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import style from './ProjectImageBar.module.css';
+import image from './../assets/images/P1.png';
 
 const ProjectImageBar = () => {
   return (
     <>
       <div className={style.containerOne}>
-        <div className={style.projectOneImage}>Hello</div>
+        <img src={image} alt='Project1' className={style.projectOneImage} />
+
         <div className={style.projectOneText}>Hello</div>
       </div>
       <div className={style.containerTwo}>
