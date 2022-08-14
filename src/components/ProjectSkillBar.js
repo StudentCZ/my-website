74c8dfbe1ed64f665c8f1cf519a73acc603c9db0
Chao Zhou <Czhou0k@gmail.com>
2022-08-13 20:57:52 -0400

    change name of class in projectskillbar component and change css to display flex

--- src/components/ProjectSkillBar.js
+++ src/components/ProjectSkillBar.js
@@ -4,25 +4,23 @@ import style from './ProjectSkillBar.module.css';
 const ProjectSkillBar = () => {
   return (
     <>
-      <div className={style.skillBar}>
-        <div className={style.skillContainer}>
-          <div className={style.skillCol1}>
-            <div>
-              <h2>Javascript</h2>
-              <div className='w3-light-grey w3-round'>
-                <div
-                  className='w3-container w3-round w3-blue'
-                  style={{ width: '25' }}
-                >
-                  25%
-                </div>
+      <div className={style.container}>
+        <div className={style.skillCol1}>
+          <div>
+            <h2>Javascript</h2>
+            <div className='w3-light-grey w3-round'>
+              <div
+                className='w3-container w3-round w3-blue'
+                style={{ width: '25' }}
+              >
+                25%
               </div>
-              <h2>HTML</h2>
             </div>
+            <h2>HTML</h2>
           </div>
-          <div className={style.skillCol2}>
-            <h2>React</h2>
-          </div>
+        </div>
+        <div className={style.skillCol2}>
+          <h2>React</h2>
         </div>
       </div>
       <div className={style.lineDiv}>
