b70126702c3fd39180daf45e1c9b5459064f1745
Chao Zhou <Czhou0k@gmail.com>
2022-08-16 21:58:14 -0400

    install hash router for npm for component linking

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import style from './NavBar.module.css';
+import About from './About';
 
 const NavBar = () => {
   return (
     <div className={style.div}>
       <li className={style.li}>
-        <Link className={style.link} to='/'>
+        <a className={style.link} href={<About />}>
           ABOUT
-        </Link>
+        </a>
       </li>
       <li className={style.li}>
         <Link className={style.link} to='/project'>
