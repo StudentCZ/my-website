31f90e036c3f590e64e62f3767cfa1bc022f1d4d
Chao Zhou <Czhou0k@gmail.com>
2022-08-18 21:02:27 -0400

    got rid of unused css in projectskillbar

--- src/components/ProjectSkillBar.js
+++ src/components/ProjectSkillBar.js
@@ -7,13 +7,7 @@ const ProjectSkillBar = () => {
       <div className={style.container}>
         <div className={style.skillCol1}>
           <div>
-            <h2>Javascript</h2>
-          </div>
-          <div
-            className='w3-container w3-round w3-blue'
-            style={{ width: '25' }}
-          >
-            25%
+            <h2 className={style.skill}>Javascript</h2>
           </div>
         </div>
       </div>
