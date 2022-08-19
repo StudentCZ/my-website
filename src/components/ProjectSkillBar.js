5a701ab2e98bad2eb1511de11999ef4a8054e702
Chao Zhou <Czhou0k@gmail.com>
2022-08-18 21:50:42 -0400

    added a span to progressbar

--- src/components/ProjectSkillBar.js
+++ src/components/ProjectSkillBar.js
@@ -9,7 +9,9 @@ const ProjectSkillBar = () => {
           <h2 className={style.skill}>Javascript</h2>
         </div>
         <div className={style.progress}>
-          <div className={style.progressBar}>75%</div>
+          <div className={style.progressBar}>
+            <span>75%</span>
+          </div>
         </div>
       </div>
     </>
