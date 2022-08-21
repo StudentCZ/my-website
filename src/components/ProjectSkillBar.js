ee26b4a34c18f6e9f7697ab26440ba5c6059e400
Chao Zhou <Czhou0k@gmail.com>
2022-08-21 19:17:43 -0400

    got a working grid and added padding to it

--- src/components/ProjectSkillBar.js
+++ src/components/ProjectSkillBar.js
@@ -13,6 +13,54 @@ const ProjectSkillBar = () => {
             </div>
           </div>
         </div>
+        <div>
+          <h2 className={style.skill}>HTML</h2>
+          <div className={style.progress}>
+            <div className={style.progressBar}>
+              <span className={style.progressBarText}>75%</span>
+            </div>
+          </div>
+        </div>
+        <div>
+          <h2 className={style.skill}>CSS</h2>
+          <div className={style.progress}>
+            <div className={style.progressBar}>
+              <span className={style.progressBarText}>75%</span>
+            </div>
+          </div>
+        </div>
+        <div>
+          <h2 className={style.skill}>React</h2>
+          <div className={style.progress}>
+            <div className={style.progressBar}>
+              <span className={style.progressBarText}>75%</span>
+            </div>
+          </div>
+        </div>
+        <div>
+          <h2 className={style.skill}>Express</h2>
+          <div className={style.progress}>
+            <div className={style.progressBar}>
+              <span className={style.progressBarText}>75%</span>
+            </div>
+          </div>
+        </div>
+        <div>
+          <h2 className={style.skill}>Redux</h2>
+          <div className={style.progress}>
+            <div className={style.progressBar}>
+              <span className={style.progressBarText}>75%</span>
+            </div>
+          </div>
+        </div>
+        <div>
+          <h2 className={style.skill}>PostgreSQL</h2>
+          <div className={style.progress}>
+            <div className={style.progressBar}>
+              <span className={style.progressBarText}>75%</span>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
