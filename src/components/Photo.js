e835b12c212af1fcd4a904c85006e571fa8eb2bb
Chao Zhou <Czhou0k@gmail.com>
2022-08-23 10:17:13 -0400

    added text to my textbox in photo component

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -9,7 +9,13 @@ const Photo = () => {
         <img className={style.image} src={WorkPic} alt='myself' />
         <div className={style.textBox}>
           <span className={style.header}>HI, I'M CHAO</span>
-          <p className={style.text}>Hello</p>
+          <p className={style.text}>
+            Having worked with a diverse population including people with
+            special needs throughout my working career, learning how to work
+            with others, communciate efficiently, compassion, empathy, being
+            patient and learning from others. I believe these skills will
+            translate well into the tech industry.{' '}
+          </p>
         </div>
       </div>
     </div>
