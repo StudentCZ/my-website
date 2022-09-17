ab3412bab180241d9e465a227a01952a6b3bebc4
Chao Zhou <Czhou0k@gmail.com>
2022-09-17 14:31:59 -0400

    changed photo of self

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import WorkPic from '../assets/images/Workpic1.jpg';
+import WorkPic from '../assets/images/new-photo.png';
 import style from './Photo.module.css';
 
 const Photo = () => {
@@ -10,12 +10,12 @@ const Photo = () => {
         <div className={style.textBox}>
           <h1 className={style.header}>HI, I'M CHAO</h1>
           <p className={style.text}>
-            I am a programmer from New York City. I had experience working
-            with a diverse population including people with special needs. I
-            have developed the skills needed to work well with others,
-            communicate effectively, be patient and empathetic, and be open to
-            others ideas and insights. As a result, working in the tech industry
-            will allow me to have a great and positive impact on many.
+            I am a programmer from New York City. I had experience working with
+            a diverse population including people with special needs. I have
+            developed the skills needed to work well with others, communicate
+            effectively, be patient and empathetic, and be open to others ideas
+            and insights. As a result, working in the tech industry will allow
+            me to have a great and positive impact on many.
           </p>
         </div>
       </div>
