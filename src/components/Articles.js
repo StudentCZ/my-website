d0b2a1960539b8c4991fea87f7ee6580d2019f99
Chao Zhou <Czhou0k@gmail.com>
2022-11-09 21:08:43 -0500

    changed href link to correct article

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -23,7 +23,7 @@ const Articles = () => {
 
       <div className={style.borderBox}>
         <a
-          href='https://medium.com/@Chao.Zhou/5-things-i-would-havedone-differently-if-i-had-to-do-the-bootcamp-again-4dfe3cb95e86'
+          href='https://medium.com/@Chao.Zhou/what-i-learned-from-learning-python-650c318e3bef'
           rel='noreferrer'
           target='_blank'
         >
