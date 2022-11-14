a16c148d900df76b3833cc8fe02a151fe513b1ba
Chao Zhou <Czhou0k@gmail.com>
2022-11-14 13:04:59 -0500

    added class to h1 and stlye

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,7 +4,7 @@ import style from './Resume.module.css';
 const Resume = () => {
   return (
     <div id='resume'>
-      <h1>RESUME</h1>
+      <h1 className={style.headingText}>RESUME</h1>
     </div>
   );
 };
