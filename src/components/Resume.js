93ab84957a6bc2ffa15192755882c3bcb66174a9
Chao Zhou <Czhou0k@gmail.com>
2022-11-16 17:32:54 -0500

    changed border box

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -7,9 +7,8 @@ const Resume = () => {
       <div>
         <h1 className={style.headingText}>RESUME</h1>
       </div>
-      <div className={style.borderBox}>
-        <div>Resume</div>
-      </div>
+
+      <div className={style.borderBox}>Resume</div>
     </div>
   );
 };
