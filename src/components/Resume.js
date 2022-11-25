91376b52e7906d19d8a38c2576d1ad2c52aa9ce1
Chao Zhou <Czhou0k@gmail.com>
2022-11-25 11:05:15 -0500

    changing id of resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,7 +3,7 @@ import style from './Resume.module.css';
 
 const Resume = () => {
   return (
-    <div className={style.contact} id='contact'>
+    <div className={style.contact} id='resume'>
       <div className={style.contactBox}>
         <div className={style.headingBox}>
           <span className={style.headingText}>CONTACT ME</span>
