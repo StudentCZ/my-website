5a747cf6c9469f0b022d1565912cf73b24dcec5d
Chao Zhou <Czhou0k@gmail.com>
2023-01-20 03:45:08 -0500

    added link to resume icon

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -6,11 +6,17 @@ const Resume = () => {
     <div className={style.contact} id='resume'>
       <div className={style.borderBox}>
         <div className={style.contactImagesBox}>
-          <img
-            className={style.images}
-            src='https://t3.ftcdn.net/jpg/03/77/85/04/360_F_377850455_Gk0rRBzegH6YX9SZK9YbgyYyLwrVb9zi.jpg'
-            alt='resume'
-          />
+          <a
+            href='https://drive.google.com/file/d/1fTZoZogTlQbULYp4Ef_chynv8gRNLfCl/view?usp=sharing'
+            rel='noreferrer'
+            target='_blank'
+          >
+            <img
+              className={style.images}
+              src='https://t3.ftcdn.net/jpg/03/77/85/04/360_F_377850455_Gk0rRBzegH6YX9SZK9YbgyYyLwrVb9zi.jpg'
+              alt='resume'
+            />
+          </a>
         </div>
         <div className={style.headingBox}>
           <span className={style.headingText}>RESUME</span>
